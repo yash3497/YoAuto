8a295783b67704a38801e7ae0df5fb6278a00ba8
Farhana Akter <106755038+Farhana007@users.noreply.github.com>
2023-01-09 08:39:47 -0500

    Update wallet_screen.dart

--- lib/screens/wallet/wallet_screen.dart
+++ lib/screens/wallet/wallet_screen.dart
@@ -1,10 +1,154 @@
 import 'package:flutter/material.dart';
 
-class WalletScreen extends StatelessWidget {
-  const WalletScreen({super.key});
+
+void main (){
+  runApp(MaterialApp(
+    home:WalletScreen()
+  ));
+}
+
+class WalletScreen extends StatefulWidget {
+  const WalletScreen({Key? key}) : super(key: key);
 
   @override
+  State<WalletScreen> createState() => _WalletScreenState();
+}
+
+class _WalletScreenState extends State<WalletScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return  Scaffold(
+      backgroundColor: Colors.white38,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: [
+                   Text("₹ XXX",style: TextStyle(
+                     color: Color.fromRGBO(1, 120, 13, 1),
+                     fontWeight: FontWeight.w700,
+                     fontSize: 46
+                   ),),
+                   Text("Wallet Money",style: TextStyle(
+                       color: Colors.black,
+                       fontWeight: FontWeight.w700,
+                       fontSize: 26) )
+                 ],
+               ),
+              height: 200,
+              width: 428,
+              color: Color.fromRGBO(255, 203, 32, 1),
+            ),
+            SizedBox(height: 50,),
+
+            Container(
+              height: 75,
+              width: 340,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                 borderRadius: BorderRadius.circular(15),
+                boxShadow:[
+                  BoxShadow(
+                     color: Color.fromRGBO(0, 0, 0, 0.1),
+                    spreadRadius: 5,
+                    blurRadius: 5
+                  )
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Text("Add Wallet Money",style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: IconButton(
+                        onPressed: (){},
+                        icon: Icon(Icons.add_circle_rounded,color: Color.fromRGBO(1, 120, 13, 1),size: 40,)
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              height: 75,
+              width: 340,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow:[
+                  BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.1),
+                      spreadRadius: 5,
+                      blurRadius: 5
+                  )
+                ],
+              ),
+              child: Center(
+                child: TextButton(
+                  onPressed: (){},
+                  child: Text("Link UPI",style: TextStyle(
+                    color: Color.fromRGBO(1, 120, 13, 1),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20
+                  ),),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 150,),
+            Container(
+              margin: EdgeInsets.only(bottom: 30),
+              height: 75,
+              width: 340,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow:[
+                  BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.1),
+                      spreadRadius: 5,
+                      blurRadius: 5
+                  )
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Text("View Wallet Transaction",style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: IconButton(
+                        onPressed: (){},
+                        icon: Icon(Icons.history,color: Color.fromRGBO(1, 120, 13, 1),size: 40,)
+                    ),
+                  )
+                ],
+            ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
