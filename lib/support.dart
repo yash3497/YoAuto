5f36c81bbb9d078de24a9485168e97545fb6965d
Farhana Akter <106755038+Farhana007@users.noreply.github.com>
2023-04-10 10:31:03 -0400

    Support Page Updated

--- lib/support.dart
+++ lib/support.dart
@@ -3,70 +3,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:yoauto_task/screens/home/mainScreen.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'constants/colors.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SupportPage extends StatelessWidget {
   const SupportPage({Key? key}) : super(key: key);
-
-  Container makeConr(
-    String t1,
-    String t2,
-  ) {
-    return Container(
-      height: 90,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(14),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26.withOpacity(0.5),
-              blurRadius: 4,
-            )
-          ]),
-      child: Expanded(
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 16, top: 10),
-                  child: Text(
-                    "$t1",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "$t2",
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.green.shade900),
-                      )),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         leading:IconButton(
-           padding: EdgeInsets.only(left: 15),
-          onPressed: (){
-
-          },
-          icon: Icon(Icons.arrow_back,
-            color: Colors.black,size: 35,
-
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 15),
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 35,
           ),
         ),
         title: Text(
@@ -75,7 +29,7 @@ class SupportPage extends StatelessWidget {
               fontSize: 26, color: Colors.black, fontWeight: FontWeight.w900),
         ),
         titleSpacing: 20,
-        backgroundColor: Colors.yellow.shade600,
+        backgroundColor: Colors.yellow[700],
       ),
       body: Container(
         color: Colors.grey[200],
@@ -91,7 +45,7 @@ class SupportPage extends StatelessWidget {
                     fit: BoxFit.fitWidth,
                   ),
                 ),
-                color: Colors.yellow[300],
+                color: Colors.yellow[600],
               ),
               Padding(
                 padding: EdgeInsets.all(15),
@@ -101,15 +55,62 @@ class SupportPage extends StatelessWidget {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15),
-                child: makeConr("Email Us at", "yoauto.help@gmail.com"),
-              ),
-              SizedBox(
-                height: 20,
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  elevation: 8,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      trailing: Icon(Icons.mark_as_unread,size: 30,color: Colors.black,),
+                      title: "Email Us At"
+                          .text
+                          .semiBold
+                          .size(17)
+                          .color(darkGreen)
+                          .make(),
+
+                      subtitle: GestureDetector(
+                          onTap: () async {
+                            final email = 'yoauto@gmail.com';
+                            final Uri launchUri = Uri(
+                              scheme: 'mailto',
+                              path: email,
+                            );
+                            await launchUrl(launchUri);
+                          },
+                          child: "yoauto@gmail.com".text.size(22).black.make()),
+                    ),
+                  ),
+                ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15),
-                child: makeConr("Call Us at", "XXXXXXXXXXXXX"),
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  elevation: 8,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      trailing: Icon(Icons.phone_enabled_rounded,size: 30,color: Colors.black,),
+                      title: "Call Us At"
+                          .text
+                          .semiBold
+                          .size(17)
+                          .color(darkGreen)
+                          .make(),
+                      subtitle: GestureDetector(
+                          onTap: () async {
+
+                            final phoneNumber = '+7260009482';
+                            final Uri launchUri = Uri(
+                              scheme: 'tel',
+                              path: phoneNumber,
+                            );
+                            await launchUrl(launchUri);
+                          },
+                          child: "7260009482".text.size(22).black.make()),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
