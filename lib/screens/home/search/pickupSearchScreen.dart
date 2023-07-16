1a8dcd06106c4c8daae40c646939c523d21d2509
Karmaxthetic <shaikhnihal728@gmail.com>
2023-07-16 15:17:54 +0530

    Demo Location Data for testing

--- lib/screens/home/search/pickupSearchScreen.dart
+++ lib/screens/home/search/pickupSearchScreen.dart
@@ -54,14 +54,20 @@ class PickupSearchDelegate extends SearchDelegate {
 
   // Demo list to show querying
   List<String> searchTerms = [
-    "Terminal 2, Chatrapati Shivaji Maharaj International Airport, Mumbai",
     "Bandra, Railway Station West, Mumbai",
     "Colaba, Gateway Of India, Mumbai",
     "Bandstand, Bandra West, Mumbai",
     "Chatrapati Shivaji Maharaj Terminus, Fort, Mumbai",
     "Juhu Beach, Mumbai",
-    "Lower Parel Railway Station East, Mumbai",
     "Haji Ali, Malabar Hill, Mumbai"
+  ];
+  List<LatLng> demoCoordinates = const [
+    LatLng(19.0596, 72.8295),
+    LatLng(18.9067, 72.8147),
+    LatLng(19.049029, 72.820647),
+    LatLng(18.9398, 72.8354),
+    LatLng(19.1075, 72.8263),
+    LatLng(18.9827, 72.8089)
   ];
 
   // first overwrite to
@@ -148,16 +154,18 @@ class PickupSearchDelegate extends SearchDelegate {
                   var prefs = await SharedPreferences.getInstance();
                   pickupLocation = query;
                   Coordinates coordinates = await latlongFromAddress();
-                  pickupLat = coordinates.latitude!;
-                  pickupLng = coordinates.longitude!;
-                  Get.offAll(MainScreen(),
+                  // pickupLat = coordinates.latitude!;
+                  // pickupLng = coordinates.longitude!;
+                  pickupLat = demoCoordinates[index].latitude;
+                  pickupLng = demoCoordinates[index].longitude;
+                  Get.offAll(const MainScreen(),
                       transition: Transition.fade,
-                      duration: Duration(milliseconds: 300));
+                      duration: const Duration(milliseconds: 300));
                 },
                 child: ListTile(
                   title: Text(result),
                   tileColor: Vx.gray100,
-                  leading: Icon(Icons.location_city),
+                  leading: const Icon(Icons.location_city),
                   minLeadingWidth: 10,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12)),
@@ -183,6 +191,14 @@ class DropSearchDelegate extends SearchDelegate {
     "Juhu Beach, Mumbai",
     "Lower Parel Railway Station East, Mumbai",
     "Haji Ali, Malabar Hill, Mumbai"
+  ];
+  List<LatLng> demoCoordinates = const [
+    LatLng(19.0596, 72.8295),
+    LatLng(18.9067, 72.8147),
+    LatLng(19.049029, 72.820647),
+    LatLng(18.9398, 72.8354),
+    LatLng(19.1075, 72.8263),
+    LatLng(18.9827, 72.8089)
   ];
   getSuggestions() async {
     if (query.isNotEmpty) {
@@ -273,8 +289,10 @@ class DropSearchDelegate extends SearchDelegate {
             var prefs = await SharedPreferences.getInstance();
             Coordinates coordinates = await latlongFromAddress();
             dropLocation = query;
-            dropLat = coordinates.latitude!;
-            dropLng = coordinates.longitude!;
+            // dropLat = coordinates.latitude!;
+            // dropLng = coordinates.longitude!;
+            dropLat = demoCoordinates[index].latitude;
+            dropLng = demoCoordinates[index].longitude;
             Get.offAll(MainScreen(),
                 transition: Transition.fade,
                 duration: Duration(milliseconds: 300));
